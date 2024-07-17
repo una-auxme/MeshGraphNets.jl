7dac1b53cd0f1b9744a07108e4e6ab1c0774757a
Julian Trommer <julian.trommer@uni-a.de>
2024-07-17 16:17:27 +0200

    Added Aqua.jl to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,20 @@
 
 using MeshGraphNets
 using Test
+using Aqua
 
 @testset "MeshGraphNets.jl" begin
     # TODO
+    @testset "Aqua.jl" begin
+        # Ambiguities in external packages
+        @testset "Method ambiguity" begin
+            Aqua.test_ambiguities([MeshGraphNets])
+        end
+        Aqua.test_all(MeshGraphNets; ambiguities = false, piracies = false)
+
+        # Piracy due to Zygote pullback
+        # @testset "Piracy" begin
+        #     Aqua.test_piracies(MeshGraphNets)
+        # end
+    end
 end
